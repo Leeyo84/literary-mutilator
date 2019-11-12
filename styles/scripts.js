07c17a11e3bd9a47157a036b5b51393e4e888535
Leonela Gutierrez <leonela_gutierrez@hotmail.com>
2019-11-11 22:16:31 -0700

    was able to change font on mouse over for para1

---
+++ styles/scripts.js
@@ -0,0 +1,9 @@
+
+function changeFont() {
+	document.getElementById("para1").style.fontFamily = "monospace";
+}
+
+function changeColor() {
+	document.getElementById(para2).style.background = "orange";
+
+}
